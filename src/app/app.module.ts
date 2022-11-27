36256500c1e861f4f980a798ecc78c6fcef8cce3
geros78 <alejo.o.o1000@gmail.com>
2022-11-27 11:40:22 -0500

    Agregada la primera version del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { BodyComponent } from './Components/body/body.component';
+import { AsideComponent } from './Components/aside/aside.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { ServicioComponent } from './components/servicio/servicio.component';
+import { TestimonioComponent } from './components/testimonio/testimonio.component';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+
+const routes: Routes =[
+  {path: 'body', component: BodyComponent},
+  {path: 'aside', component: AsideComponent},
+  {path: 'contacto', component: ContactosComponent},
+  {path: 'servicio', component: ServicioComponent},
+  {path: 'testimonio', component: TestimonioComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    BodyComponent,
+    AsideComponent,
+    ContactosComponent,
+    ServicioComponent,
+    TestimonioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    YouTubePlayerModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
